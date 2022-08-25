ebec08b55a12eefe8063d0b15df24ad9f6ea8467
mordulu <109992564+mordulu@users.noreply.github.com>
2022-08-25 23:38:11 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace operatorler
+
+{
+
+    class Program
+   {
+
+        static void Main(string[] args)
+       {
+      //      try
+    //        {
+    //           Console.WriteLine("Bir sayi giriniz:");
+    //           int sayi = Convert.ToInt32(Console.ReadLine());
+   //
+   //            Console.WriteLine("Girmiş olduğunuz sayi :" + sayi);
+    //        }
+    //        catch(Exception ex)
+    //        {
+    //           Console.WriteLine("Hata: "+ ex.Message.ToString());
+    //        }
+           // finally   
+           //  {
+
+           //    Console.WriteLine("İşlem tamamlandı");  
+           //}
+           try
+           {
+              //  int a = int.Parse("test");
+                int a = int.Parse("-20000000000");
+           }
+           catch (ArgumentException ex)
+           {
+            Console.WriteLine ("Boş değer girdiniz");
+            Console.WriteLine(ex);
+           }  
+             catch(FormatException ex)
+           {
+            Console.WriteLine ("Veri tipi uygun değil");
+            Console.WriteLine(ex);
+
+           }
+            catch(OverflowException ex)
+            {
+               Console.WriteLine("Çok küçük yada çok büyük bir değer girdiniz.");
+               Console.WriteLine(ex);
+
+            }
+        }
+    }         
+}
